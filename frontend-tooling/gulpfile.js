a48688160e341462440a9a354b2031108751c6f8
Brian Hernandez <brianhernandez@users.noreply.github.com>
2018-06-01 14:27:02 -0400

    Committing changes from version: 1.3.29 to version: 1.3.30.

--- frontend-tooling/gulpfile.js
+++ frontend-tooling/gulpfile.js
@@ -77,20 +77,20 @@ gulp.task('bump', function(){
 });
 
 gulp.task('gitAdd', ['bump'], function (cb) {
-  console.log(exec('git add .', function (err, stdout, stderr) {
-    // console.log(stdout);
-    // console.log(stderr);
+  exec('git add .', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
     cb(err);
-  }));
+  });
 });
 
 gulp.task('gitCommit', ['gitAdd'], function (cb) {
   newProjectVersion = getPackageJson();
-  console.log(exec('git commit -m "Committing changes from version: ' + oldProjectVersion + ' to version: ' + newProjectVersion + '."' , function (err, stdout, stderr) {
-    // console.log(stdout);
-    // console.log(stderr);
+  exec('git commit -m "Committing changes from version: ' + oldProjectVersion + ' to version: ' + newProjectVersion + '."' , function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
     cb(err);
-  }));
+  });
 });
 
 gulp.task('deploy', ['gitCommit'], function(){
